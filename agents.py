8f27f5fc42a708887089ae68c926c83e431977fd
nadiyah26 <nadiyah@springboards.ai>
2026-04-13 22:46:25 +1000

    troubleshooting

--- agents.py
+++ agents.py
@@ -7,6 +7,7 @@ from google.genai import types
 import os
 from dotenv import load_dotenv
 import json
+import httpx
 
 load_dotenv()
 
@@ -15,8 +16,7 @@ load_dotenv()
 tavily_client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
 gemini_client = genai.Client(api_key = os.getenv("GEMINI_API_KEY"))
 
-ngrok_headers = {"Authorization": f"Bearer {os.getenv('NGROK_AUTH_TOKEN')}",
-                 "Ngrok-Skip-Client-Validation": "true"}
+http_client = httpx.Client({"ngrok-Skip-Client-Validation": "true"}, verify = False)
 
 class socratic_ai_tutor:
     def __init__(self, concept, learning_style, model1 = "gemma3:1b", model2 = "qwen3.5"):
@@ -24,7 +24,7 @@ class socratic_ai_tutor:
             self.llm = ChatOllama(model=model1, 
                               validate_model_on_init=True, 
                               base_url = "https://socratic-ai-ollama.onrender.com",
-                              client_kwargs = ngrok_headers)
+                              http_client = http_client)
             
         except Exception as e:
             raise RuntimeError(f"Error initializing LLM: {e}")
